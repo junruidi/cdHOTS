23671abbaf349f7b7f152d8698f1b1cb88e35613
Junrui Di <jdi2@jhu.edu>
2018-05-20 19:54:27 -0400

    1st

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+#' third order mgf
+#' @param x a matrix
+#' @return third order
+#' @export
+#'
+hello<- function (x) {
+  n <- nrow (x)
+  m <- ncol (x)
+  mm <- 1 : m
+  nn <- 1 : n
+  c3 <- array (0, c (m, m, m))
+  for (i in nn) {
+    c3 <- c3 + outer (outer (x [i, ], x [i, ]), x [i,])
+  }
+  return (c3)
+}
